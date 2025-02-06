d6b55575078780c98e56538ddc46c6584d9b0047
Limessh <elizaveta.d.sh@gmail.com>
2025-02-06 21:43:11 +0300

    Up-1

---
+++ lab7.1.py
@@ -0,0 +1,20 @@
+import requests
+
+def get_weather(city, key):
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={key}&units=metric"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        result = response.json()
+        country = result['sys']['country']
+        temperature = result['main']['temp']
+        pressure = result['main']['pressure']
+        humidity = result['main']['humidity']
+        print(f"Погода в городе: {city}, {country}\nТемпература: {temperature} °C \nДавление: {pressure} гПа \nВлажность: {humidity}%")
+    else:
+        print(f"Ошибка: Город не найден!")
+
+if __name__ == "__main__":
+    city = input("Введите название города: ")
+    key = "f48c07d166749ecdc554b4c870a74cbe"  
+    get_weather(city, key)
---
+++ lab7.2.py
@@ -0,0 +1,28 @@
+import requests
+from datetime import datetime, timezone
+
+def get_text():
+    url = "http://api.open-notify.org/iss-now.json"
+    astronauts_url = "http://api.open-notify.org/astros.json"
+
+    iss_response = requests.get(url)
+    astronauts_response = requests.get(astronauts_url)
+
+    iss_result = iss_response.json()
+    astronauts_result = astronauts_response.json()
+
+    location = iss_result['iss_position']
+    time = iss_result['timestamp']
+    astronauts = astronauts_result['people']
+    readable_time = datetime.fromtimestamp(time, timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
+    
+    print(f"Текущая информация о местоположении МКС: \nВремя обновления: {readable_time} UTC \nМестоположение МКС: Широта: {location['latitude']}° N, Долгота: {location['longitude']}° E")
+    if len(astronauts) > 0:
+        print("\nЛюди в космосе:")
+        for astronaut in astronauts:
+            print(f"- {astronaut['name']} на {astronaut['craft']}")
+    else:
+        print("Сейчас нет людей в космосе.")
+    
+if __name__ == "__main__":
+    get_text()
